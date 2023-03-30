fdc08109ad031b96415a23f44120fef783b95ee1
Apenas Esdras <93005998+ApenasEsdras@users.noreply.github.com>
2023-03-30 11:35:12 -0300

    atualização

--- js/implementandoTabelaHash.js
+++ js/implementandoTabelaHash.js
@@ -1,4 +1,7 @@
 /**
+ * https://www.freecodecamp.org/news/javascript-hash-table-associative-array-hashing-in-js/
+ * 
+ * 
  * Você pode implementar uma tabela Hash em JavaScript em três etapas:
 
 ## Crie um HashTable classe com table e size propriedades iniciais
@@ -11,6 +14,7 @@ $$ Adicione o set() e get() métodos para adicionar e recuperar pares de chave /
 // Propriedades iniciais da classe HashTable
 class HashTable {
   constructor() {
+    // retornar um número entre 0 and 127.
     this.table = new Array(127);
     this.size = 0;
   }
@@ -21,12 +25,15 @@ class HashTable {
     for (let i = 0; i < Key.length; i++) {
       hash += Key.charCodeAt(i);
     }
+    /*
+    Para garantir que o valor do hash não exceda o tamanho estabelecido usamos o operador do módulo para limita-lo.
+    */
     return hash % this.table.length;
   }
 
   /*
 ## O set() método chamará o _hash() método para obter o index valor.
-## O [key, value] par será atribuído ao table no especificado index
+## O [key, value] serve para atribuído ao table no especificado index
 ## Então, o size propriedade será incrementada por um
 */
 
@@ -38,15 +45,18 @@ class HashTable {
 
   // get() é um método para recuperar um valor por sua chave
   /*
-## O método chamará o _hash() método para recuperar novamente a tabela index
-## Retornar o valor armazenado em table[index]
-*/
+    ## O método chamará o _hash() método para recuperar novamente a tabela index
+    ## Retornar o valor armazenado em table[index]
+  */
 
+  // get == ler
+  // retorno o par de chave/valor
   get(key) {
     const index = this._hash(key);
     return this.table[index];
   }
 
+  // Para excluir um par de chave / valor da Hash Table, você precisa escrever um remove() método que aceita um key valor como seu parâmetro
   // === método remove ( ) === //
   remove(key) {
     const index = this._hash(key);
@@ -59,24 +69,123 @@ class HashTable {
       return false;
     }
   }
+
+  // segunda parte
+
+  set(key, value) {
+    const index = this._hash(key);
+    if (this.table[index]){
+      for (let i = 0; i < this.table[index].length; i++) {
+      // Encontra o par chave/valor na cadeia
+        if (this.table[index][i][0] === key) {
+          this.table[index][1] = value;
+          return;
+        }
+      }
+  // não encontrado, empurre um novo par chave/valor
+      this.table[index].push([key, value]);
+    }else{
+      this.table[index] = [];
+      this.table[index].push([key, value]);
+    }
+    this.size++;
+  };
+
+  /*
+  Em seguida, atualize o get() método para verificar também a matriz de segundo nível com uma for loop e retorne o par de chave / valor correto:
+  */
+  
+  get(key) {
+    const index = this._hash(key);
+    if (this.table[index]) {
+      for (let i = 0; i < this.table.length; i++) {
+        if (this.table[index][i][0] === key) {
+          return this.table[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  };
+
+  /*
+  Finalmente, você precisa atualizar o remove() método para que ele passe por cima da matriz de segundo nível e remova a matriz com a direita key valor usando o splice() método
+  */
+
+  remove(key){
+    const index = this._hash(key);
+    if (this.table[index] && this.table[index].length) {
+      for (let i = 0; i < this.table.length; i++) {
+        if (this.table[index][i][0] === key) {
+          this.table[index].splice(i, 1);
+          this.size--;
+          return false;
+        }
+      }
+    }else{
+      return false;
+    }
+  }
+
+  /*
+  Com isso, seu HashTable a classe poderá evitar qualquer colisão de número de índice e armazenar o par de chave / valor dentro da matriz de segundo nível.
+  */ 
+//=====================//
+
+display(){
+  this.table.forEach((values, index) => {
+    const chainedValues = values.map(
+      ([key, value]) => `[${key}: ${value} ]`
+    );
+    console.log(`${index}: ${chainedValues}`);
+  });7
+}
 }
 //  Teste do método HashTable ( )
 const ht = new HashTable();
 
-ht.set("Canada", 300);
-ht.set("erace", 100);
-ht.set("spain", 110);
+ht.set("France", 111);
+ht.set("Spain", 150);
+ht.set("ǻ", 192);
 
-// extrair dados
-console.log(ht.get("Canada"));
-console.log(ht.get("erace"));
-console.log(ht.get("spain"));
+ht.display();
+// 83: [ France: 111 ]
+// 126: [ Spain: 150 ],[ ǻ: 192 ]
 
-// remover dados
-console.log(ht.get("Canada"));
-console.log(ht.get("Canada"));
+console.log(ht.size); // 3
+ht.remove("Spain");
+ht.display();
+// 83: [ France: 111 ]
+// 126: [ ǻ: 192 ]
+/*
+dados atuais
+[
+    [ "Spain", 110],
+    [ "France", 100]
+]
+... 
+Para lidar com o index colisão de números, você precisa armazenar o par de chave / valor em uma segunda matriz para que o resultado final seja o seguinte:
 
-//inserir novos dados
+[
+    [
+        [ "Spain", 110 ],
+        [ "ǻ", 192 ]
+    ],
+    [
+        ["France", 100]
+    ],
+]
+ ## Para criar a segunda matriz, você precisa atualizar a set() método para que:
 
-console.log(ht.get("Canada"));
-console.log(ht.get("Canada"));
+Olhe para o table[index] e faça um loop sobre os valores da matriz.
+#1 - Se a chave em uma das matrizes for igual à key passado para o método, 
+substitua o valor no índice 1 e interromper qualquer execução adicional
+com o return declaração.
+
+#2 - Se não houver correspondência key é encontrado, pressione uma nova matriz
+de chave e valor para a segunda matriz. Senão, inicialize uma nova matriz 
+e empurre o par de chave / valor para o especificado index
+Sempre que um push() método é chamado, incremente o size propriedade por um.
+
+
+O completo set() o código do método será o seguinte:
+*/
--- js/intruductionInHashTable.js
+++ js/intruductionInHashTable.js
@@ -4,10 +4,10 @@ let obj = {
   Esdras: "555-0105",
   Soares: "555-0105",
 };
+// sexibe o obj
 console.log(obj);
 
 // o Object protótipo tem o hasOwnProperty() método que permite verificar se uma propriedade não é herdada
-
 const obj1 = {};
 obj1.name = "Esdras";
 // Ex de chamada de método herdado de objeto JavaScript
@@ -15,14 +15,17 @@ console.log(obj1.hasOwnProperty("name")); // retorna true
 
 /* ================================ */
 
-// Map permite armazenar pares de valores-chave dentro da estrutura de dados.
+// Map() permite armazenar pares de valores-chave dentro da estrutura de dados.
 
 const collection = new Map();
 collection.set("Esdras", "555-8787");
 collection.set("Soares", "8787-555");
-
 console.log(collection.get("Esdras"));
+console.log(collection.get("Soares"));
+
 console.log(collection.size);
+
+// =================================== //
 
 // utilizando loop com Map()
 const myMap = new Map();
